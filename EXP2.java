a9a106cd9b1e701cc271400d54c007bcc71602b9
gpreccse <araghunath2373@gmail.com>
2024-05-27 23:19:27 +0530

    Update EXP2.java

--- EXP2.java
+++ EXP2.java
@@ -1,81 +1,68 @@
 //2. Implementation of Lexical Analyzer.
 import java.util.*;
-class EXP2
-{
-	private static final List<String> keywords = Arrays.asList("if","else","while","for","int","float","char","double"), operators=Arrays.asList("+","-","*","/","%",">","<",">=","<=","==","!=","&&","||","!","+=","-=","*=","/=","%=");
-	enum TokenType{
-		KEYWORD,NUMBER,IDENTIFIER,SYMBOL,OPERATOR
-	}
-	static class Token
-	{
-		TokenType type;
-		String value;
-		public Token(TokenType type,String value)
-		{
-			this.type=type;
-			this.value=value;
-		}
-		public String toString()
-		{
-			return "< "+type+" , "+value+">";
-		}
-	}
-	public static List<Token> analyze(String input)
-	{
-		List<Token> Tokens=new ArrayList<>();
-		StringBuilder ct=new StringBuilder();
-		for(char c:input.toCharArray())
-		{
-			if(Character.isWhitespace(c) && ct.length()>0)
-			{
-				Tokens.add(createToken(ct.toString()));
-				ct=new StringBuilder();
-			}
-			else if(Character.isDigit(c)||Character.isLetter(c))
-				ct.append(c);
-			else
-			{
-				if(ct.length()>0)
-				{
-					Tokens.add(createToken(ct.toString()));
-					ct=new StringBuilder();
-				}
-				if(operators.contains(c+""))
-					Tokens.add(new Token(TokenType.OPERATOR,c+""));
-				else
-					Tokens.add(new Token(TokenType.SYMBOL,c+""));
-			}
-		}
-		if(ct.length()>0)
-			Tokens.add(createToken(ct.toString()));
-		return Tokens;
-	}
-	private static Token createToken(String s)
-	{
-		if(keywords.contains(s))
-			return new Token(TokenType.KEYWORD,s);
-		else if(operators.contains(s))
-			return new Token(TokenType.OPERATOR,s);
-		else if(s.matches("[a-zA-z]+[a-zA-Z0-9]*"))
-			return new Token(TokenType.IDENTIFIER,s);
-		else if(s.matches("[0-9]+"))
-			return new Token(TokenType.NUMBER,s);
-		else
-			throw new IllegalArgumentException("Invalid Token : "+s);
-	}
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Code to Analyze and type exit to finish");
-		String input=sc.nextLine();
-		while(!input.equals("exit"))
-		{
-			List<Token> tokens = analyze(input);
-			for(Token token:tokens)
-				System.out.print(token+" ");
-			System.out.println();
-			input=sc.nextLine();
-		}
-        sc.close();
-	}
+public class exp2 {
+    private static final List<String> KEYWORDS = Arrays.asList("if", "else", "while", "for", "int", "float", "double");
+    enum TokenType {
+        IDENTIFIER, NUMBER, KEYWORD, SYMBOL
+    }
+    static class Token {
+        TokenType type;
+        String value;
+
+        public Token(TokenType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+        public String toString() {
+            return "<" + type + ", " + value + ">";
+        }
+    }
+    public static List<Token> analyze(String input) {
+        List<Token> tokens = new ArrayList<>();
+        StringBuilder currentToken = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                if (currentToken.length() > 0) {
+                    tokens.add(createToken(currentToken.toString()));
+                    currentToken = new StringBuilder();
+                }
+            } else if (Character.isLetter(c)) {
+                currentToken.append(c);
+            } else if (Character.isDigit(c)) {
+                currentToken.append(c);
+            } else {
+                if (currentToken.length() > 0) {
+                    tokens.add(createToken(currentToken.toString()));
+                    currentToken = new StringBuilder();
+                }
+                tokens.add(new Token(TokenType.SYMBOL, String.valueOf(c)));
+            }
+        }
+        if (currentToken.length() > 0) {
+            tokens.add(createToken(currentToken.toString()));
+        
+        return tokens;
+    }
+    private static Token createToken(String tokenValue) {
+        if (KEYWORDS.contains(tokenValue)) {
+            return new Token(TokenType.KEYWORD, tokenValue);
+        } else if (tokenValue.matches("[a-zA-Z]+")) {
+            return new Token(TokenType.IDENTIFIER, tokenValue);
+        } else if (tokenValue.matches("[0-9]+")) {
+            return new Token(TokenType.NUMBER, tokenValue);
+        } else {
+            throw new IllegalArgumentException("Invalid token: " + tokenValue);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter input string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        List<Token> tokens = analyze(input);
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
 }
